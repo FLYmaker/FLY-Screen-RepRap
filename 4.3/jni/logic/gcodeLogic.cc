389a90a08ef0bf3c76121d65d8da4a5ece6908dd
Resident-P <105979780+Resident-P@users.noreply.github.com>
2025-01-19 21:22:46 +0100

    Update gcodeLogic.cc
    Changes for correctly work in RRF 3.5 ,  without this changes list gcode files and macros files not loaded.

--- 4.3/jni/logic/gcodeLogic.cc
+++ 4.3/jni/logic/gcodeLogic.cc
@@ -3015,8 +3015,8 @@ static void onListItemClick_ListView5(ZKListView *pListView, int index, int id) 
 static bool onButtonClick_Button9(ZKButton *pButton) {
    // LOGD(" ButtonClick Button9 !!!\n");
 	 currmode = 0;
-	 print_file_path = "M20 S2 P0:/gcodes";
-	 Hardware_serial_transmission("M20 S2 P0:/gcodes\r\n");
+	 print_file_path = "M20 S2 P\"0:/gcodes\"";
+	 Hardware_serial_transmission("M20 S2 P\"0:/gcodes\"\r\n");
 	 mboardsdPtr->setVisible(true);
 	 mfishPtr->setVisible(false);
     return false;
@@ -3030,7 +3030,7 @@ static bool onButtonClick_Button10(ZKButton *pButton) {
 	sprintf(macros_print_patha,"M98 P\"0:/macros/");
 	sprintf(macros_print_path,"M98 P\"0:/macros/");
 	currmode = 1;
-	 Hardware_serial_transmission("M20 S2 P0:/macros\r\n");
+	Hardware_serial_transmission("M20 S2 P\"0:/macros\"\r\n");
 	// snprintf(macros_print_path, sizeof(macros_print_path), "M98 P\"0:/macros/");
 
 	 mboardsdPtr->setVisible(true);
